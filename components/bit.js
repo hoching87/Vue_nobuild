286b1668256e5deebd48cf47ed715ce82d43e3e6
Cheah Ho Ching <hoching87@gmail.com>
2023-04-03 23:01:10 +0800

    Initial sample Vue app

---
+++ components/bit.js
@@ -0,0 +1,13 @@
+import {ref} from "../vue.esm-browser.js";
+
+export default {
+  props: {
+    title: String
+  },
+  setup(props, ctx) {
+    // work with props and emmiter here
+  },
+  methods: {},
+
+  template: "#template-button"
+};
